5a9dc2e64d6854f67ffe00e50aa21646afe407ec
Satendra sharma <satendrasharma6991@gmail.com>
2020-09-17 21:57:48 +0530

    some css fixes

--- edie-homepage-master/script.js
+++ edie-homepage-master/script.js
@@ -1,10 +1,15 @@
 const navbar = document.querySelector(".navbar");
 
 navbar.addEventListener("click", e => {
-  console.log(e.target.classList);
+  console.dir(e.target);
+
   if (e.target.classList.contains("hamburger")) {
     navbar.classList.add("navbar--mobile");
-  } else if (e.target.classList.contains("close")) {
+  } else if (
+    e.target.classList.contains("close") ||
+    e.target.classList.contains("link") ||
+    e.target.tagName === "A"
+  ) {
     navbar.classList.remove("navbar--mobile");
   }
 });
